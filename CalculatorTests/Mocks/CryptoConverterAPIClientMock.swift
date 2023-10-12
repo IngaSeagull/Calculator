2d36ab602b2927a579fb49b03787d51f2a4f6964
IngaSeagull <inga.soprun@gmail.com>
2023-10-12 11:35:51 +0200

    Fixed tests after moving Networking to SPM package

--- CalculatorTests/Mocks/CryptoConverterAPIClientMock.swift
+++ CalculatorTests/Mocks/CryptoConverterAPIClientMock.swift
@@ -1,9 +1,10 @@
 import Foundation
 @testable import Calculator
+@testable import CryptocurrencyNetworking
 
 final class CryptoConverterAPIClientMock: APIClientProtocol {
     let usdFromBitcoinResult = 26481.3
-    func getUSDFromBitcoin() async -> Result<Double, Calculator.APIError> {
+    func getUSDFromBitcoin() async -> Result<Double, APIError> {
         .success(usdFromBitcoinResult)
     }
 }
